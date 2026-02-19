2773f797ccf1d93344a8cf71f45e2ef8cb8ba16e
maxcaut <maxcaut@yahoo.it>
2026-02-19 20:39:51 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -9,6 +9,8 @@ async function caricaValore(citta) {
   valoreEl.textContent = "Caricamento dati...";
 
   let numero = parseFloat(potenzaInput.value);
+  if (isNan(numero) || numero<=0 {
+    numero = 3;}
   let fattore = numero/3;
 
   try {
